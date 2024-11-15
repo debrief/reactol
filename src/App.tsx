e9dc3cfc309570f19b4e4003988a6c3c1fee5aa0
Ian Mayo <ian@planetmayo.com>
2024-11-15 11:33:18 +0000

    Reinstate antd splitters

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Card, ConfigProvider, Flex, Splitter, Typography } from 'antd';
 import { LatLngExpression } from 'leaflet'
 import './App.css'
 import { Circle, MapContainer, Marker, Popup, TileLayer, GeoJSON, LayerGroup, LayersControl } from 'react-leaflet'
@@ -14,6 +15,14 @@ const fillBlueOptions = { fillColor: 'blue' }
 const setColor = () => {
   return { weight: 3 };
 };
+
+const Desc: React.FC<Readonly<{ text?: string | number }>> = (props) => (
+  <Flex justify="center" align="center" style={{ height: '100%' }}>
+  <Typography.Title type="secondary" level={5} style={{ whiteSpace: 'nowrap' }}>
+  {props.text}
+  </Typography.Title>
+  </Flex>
+);
 
 function App() {
   const [tracks, setTracks] = useState<React.ReactElement[]>([])
@@ -35,28 +44,61 @@ function App() {
     </LayersControl.Overlay>)
     setTracks(trackLayers)
   }, [])
+
+  const antdTheme = {
+    components: {
+      Splitter: {
+        splitBarSize: 10,
+      }
+    }
+  }
+
   return (
     <div className="App">
-        <MapContainer center={[35.505, -4.09]} zoom={8} scrollWheelZoom={true}>
-          <LayersControl position="topright">
-            <LayersControl.BaseLayer checked name='OpenStreetMap'>
-              <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-            </LayersControl.BaseLayer>
-            {tracks}
-            {points}
-            {zones}
-          </LayersControl>
-              <Circle center={center} pathOptions={fillBlueOptions} radius={200} />
-          <LayerGroup>
-          </LayerGroup>     
+       <ConfigProvider theme={antdTheme}>
 
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
+       <Splitter style={{ height: '100vh', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
+          <Splitter.Panel key='left' collapsible defaultSize="20%" min="20%" max="70%">
+            <Splitter layout="vertical" style={{ height: '100vh', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
+              <Splitter.Panel defaultSize="20%" min="10%" max="20%" resizable={true}>
+                <Desc text="Time Control" />
+              </Splitter.Panel>
+              <Splitter.Panel>
+                <Card title='Layers' style={{width: '100%', height: '100%'}} >
+                  <Desc text="Layer control here" />
+                </Card>
+              </Splitter.Panel>
+              <Splitter.Panel>
+                <Card title='Properties'>
+                  <Desc text="Property editor here" />
+                </Card>
+              </Splitter.Panel>
+            </Splitter>
+          </Splitter.Panel>
+          <Splitter.Panel key='right'>
+            <MapContainer center={[35.505, -4.09]} zoom={8} scrollWheelZoom={true}>
+            <LayersControl position="topright">
+              <LayersControl.BaseLayer checked name='OpenStreetMap'>
+                <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+              </LayersControl.BaseLayer>
+              {tracks}
+              {points}
+              {zones}
+            </LayersControl>
+                <Circle center={center} pathOptions={fillBlueOptions} radius={200} />
+            <LayerGroup>
+            </LayerGroup>     
+
+            <Marker position={[51.505, -0.09]}>
+              <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+          </Splitter.Panel>
+        </Splitter>
+      </ConfigProvider>
     </div>
   )
 }
