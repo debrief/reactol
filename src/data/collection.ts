a822561dc983d32e4a232b52e61bbb1b4da825cd
Ian Mayo <ian@planetmayo.com>
2024-11-15 13:01:16 +0000

    fix indents

--- src/data/collection.ts
+++ src/data/collection.ts
@@ -3,7 +3,6 @@ import track1 from './track1.ts'
 import track2 from './track2.ts'
 import points from './points.ts'
 import zones from './zones.ts'
- 
 
 const features: FeatureCollection = {
   type: "FeatureCollection",  
