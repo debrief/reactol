07e8b0f6e75cf03580822b14947ac377b0d16554
Ian Mayo <ian@planetmayo.com>
2024-11-15 10:23:40 +0000

    Switch to leaflet mapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { LatLngExpression } from 'leaflet'
 import './App.css'
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { Circle, MapContainer, Marker, Popup, TileLayer, GeoJSON, LayerGroup, LayersControl } from 'react-leaflet'
+import track1 from './data/track1.json'
+import track2 from './data/track2.json'
+import points from './data/points.json'
+import zones from './data/zones.json'
+import { FeatureCollection } from 'geojson'
 
 // const Desc: React.FC<Readonly<{ text?: string | number }>> = (props) => (
 //   <Flex justify="center" align="center" style={{ height: '100%' }}>
@@ -9,9 +15,8 @@ import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 //   </Flex>
 // );
 
-// const circleFeature = new Feature({
-//   geometry: new CircleGeom(transform([ -6.2685, 35.8192], 'EPSG:4326', 'EPSG:3857'), 10000 ),
-// });
+const center: LatLngExpression = [51.505, -0.09]
+const fillBlueOptions = { fillColor: 'blue' }
 
 // const vectorSource = new VectorSource({
 //   features: new GeoJSON().readFeatures(track1)
@@ -58,31 +63,39 @@ import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 //   }
 // });
 
-// const map = new OlMap({
-//   layers: [
-//     new OlLayerTile({
-//       properties: {
-//         name: 'OSM'
-//       },
-//       source: new OlSourceOSM()
-//     }),
-//     vectorLayer,
-//     pointLayer
-//   ],
-//   view: new OlView({
-//     center: transform([-3, 36], 'EPSG:4326', 'EPSG:3857'),
-//     zoom: 7,
-//   })
-// });
+const setColor = () => {
+  return { weight: 3 };
+};
 
 function App() {
   return (
     <div className="App">
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-          <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
+        <MapContainer center={[32.505, -4.09]} zoom={8} scrollWheelZoom={true}>
+          <LayersControl position="topright">
+            <LayersControl.BaseLayer checked name='OpenStreetMap'>
+              <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+              <TileLayer url='https://t1.openseamap.org/seamark/{z}/{x}/{y}.png'/>
+            </LayersControl.BaseLayer>  
+            <LayersControl.Overlay checked name='tracks'>
+              <LayersControl.Overlay checked name='track 1'>
+                <GeoJSON key='t1' data={track1 as FeatureCollection} style={setColor} />
+              </LayersControl.Overlay>
+              <LayersControl.Overlay checked name='track 2'>
+                <GeoJSON key='t2' data={track2 as FeatureCollection} style={setColor} />
+              </LayersControl.Overlay>
+            </LayersControl.Overlay>
+            <LayersControl.Overlay checked name='points'>
+              <GeoJSON key='points' data={points as FeatureCollection} style={setColor} />
+            </LayersControl.Overlay>
+            <LayersControl.Overlay checked name='zones'>
+              <GeoJSON key='zones' data={zones as FeatureCollection} style={setColor} />
+            </LayersControl.Overlay>
+          </LayersControl>
+              <Circle center={center} pathOptions={fillBlueOptions} radius={200} />
+          <LayerGroup>
+          </LayerGroup>     
+
           <Marker position={[51.505, -0.09]}>
             <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
