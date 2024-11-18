fd3e792e63a816a0bc6debb7731cf4d133c17d17
Ian Mayo <ian@planetmayo.com>
2024-11-18 16:41:05 +0000

    Refactor out track class

---
+++ src/components/Track.tsx
@@ -0,0 +1,38 @@
+import { Feature, Geometry, MultiPoint } from "geojson";
+import { LatLngExpression  } from 'leaflet'
+import { Polyline, CircleMarker } from 'react-leaflet'
+import { useAppSelector } from "../app/hooks";
+
+export interface TrackProps {
+  feature: Feature 
+}
+
+const Track: React.FC<TrackProps> = ({feature}) => {
+  const selectedFeatureId = useAppSelector(state => state.selected.selected)
+  const isSelected = feature.id === selectedFeatureId
+
+  const colorFor = (feature: Feature<Geometry, unknown> | undefined): string => {
+    if (isSelected) {
+      return '#aaa'
+    }
+    if (feature) {
+      const feat = feature as Feature
+      if (feat.properties) {
+        return feat.properties.color || '#ff0'
+      }
+    }
+    return '#000';
+  };
+  
+  const trackCoords = (feature.geometry as MultiPoint).coordinates.map(item => [item[1], item[0]]) as LatLngExpression[]
+
+  return (
+    <>
+      <Polyline key={feature.id + '-line-' + isSelected} positions={trackCoords} weight={2} color={colorFor(feature)} />
+      { trackCoords.map((item, index) => 
+        <CircleMarker key={feature.id + '-point-' + index} center={item} radius={3} color={colorFor(feature)} /> )}
+    </>
+  )
+}
+
+export default Track
