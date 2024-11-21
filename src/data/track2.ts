4f471c94c7e63c56fb89233cf0734ebb56e65f3b
Ian Mayo <ian@planetmayo.com>
2024-11-21 20:10:01 +0000

    update json

--- src/data/track2.ts
+++ src/data/track2.ts
@@ -1,24 +1,49 @@
 import { TRACK_TYPE } from "../constants";
-import { calculateCoursesAndSpeeds } from '../helpers/trackCalculations';
 
 const track = {
-  type: "Feature",
+  type: 'Feature',
   properties: {
     dataType: TRACK_TYPE,
+
     color: '#00F',
     name: 'Track 2',
     times: [
-      "2024-11-14T16:16:53.662Z",
-      "2024-11-14T16:17:53.662Z",
-      "2024-11-14T16:18:53.662Z",
-      "2024-11-14T16:19:53.662Z",
-      "2024-11-14T16:20:53.662Z",
-      "2024-11-14T16:21:53.662Z",
-      "2024-11-14T16:22:53.662Z",
-      "2024-11-14T16:23:53.662Z",
-      "2024-11-14T16:24:53.662Z",
-      "2024-11-14T16:25:53.662Z"
-    ]
+      '2024-11-14T16:16:53.662Z',
+      '2024-11-14T16:17:53.662Z',
+      '2024-11-14T16:18:53.662Z',
+      '2024-11-14T16:19:53.662Z',
+      '2024-11-14T16:20:53.662Z',
+      '2024-11-14T16:21:53.662Z',
+      '2024-11-14T16:22:53.662Z',
+      '2024-11-14T16:23:53.662Z',
+      '2024-11-14T16:24:53.662Z',
+      '2024-11-14T16:25:53.662Z'
+    ],
+    courses: [
+      68.8884245629198,
+      87.21148457316167,
+      101.19964921336205,
+      101.43720246981847,
+      73.56437457538713,
+      75.71882975705745,
+      66.3972536045927,
+      67.49473669165536,
+      71.84655459047862,
+      69.67064564106698
+    ],
+    speeds: [
+      1860.70404023613,
+      4505.867814422231,
+      2378.9683300645893,
+      2660.4486223610925,
+      2375.9079958279826,
+      2181.3438000519573,
+      2008.1351114217919,
+      2806.3157929361296,
+      3460.2973232766494,
+      3133.3065581063893
+    ],
+    visible: true
   },
   geometry: {
     coordinates: [
@@ -63,21 +88,9 @@ const track = {
         36.13
       ]
     ],
-    type: "MultiPoint"
-  }
+    type: 'MultiPoint'
+  },
+  id: 'f-2'
 }
-
-if (!track.properties) {
-  track.properties = {
-    dataType: 'track',
-    color: '#f00',
-    name: 'ALBA',
-    times: []
-  }
-}
-
-const { courses, speeds } = calculateCoursesAndSpeeds(track);
-(track.properties as any).courses = courses;
-(track.properties as any).speeds = speeds;
 
 export default track;
