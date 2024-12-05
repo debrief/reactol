9a38b578dcd1ffa282561ae160329d758a810a88
Ian Mayo <ian@planetmayo.com>
2024-12-05 11:22:31 +0000

    refactor more logic into testable method

--- src/helpers/generateCurrentLocations.ts
+++ src/helpers/generateCurrentLocations.ts
@@ -15,36 +15,40 @@ const timeVal = (timeStr: string): number => {
   return new Date(timeStr).getTime()
 }
 
-export const calcInterpLocation = (poly: MultiPoint, times: any, current: number, index: number): Position => {
-  const coords = poly.coordinates
-  const isFirst = index === 0
-  const beforeIndex = isFirst ? 0 : index - 1
-  const afterIndex = isFirst ? 0 : index
-  const beforeCoords = coords[beforeIndex]
-  const afterCoords = coords[afterIndex]
-  const before = turf.point(beforeCoords)
-  const after = turf.point(afterCoords)
-  const turfPath = turf.lineString([beforeCoords, afterCoords])
-  const len = turf.distance(before, after)
-  const beforeTime = timeVal(times[beforeIndex])
-  const afterTime = timeVal(times[afterIndex])
-  const timeDelta = afterTime - beforeTime
-  const proportion = (current - beforeTime) / timeDelta
-  const lenProp = len * proportion
-  const interpolated = isNaN(lenProp) ? before : turf.along(turfPath, lenProp)
-  const markerLoc = interpolated.geometry.coordinates
-  return markerLoc
+export const calcInterpLocation = (poly: MultiPoint, times: string[], current: number): Position | undefined => {
+  const index = times.findIndex((time: string) => new Date(time).getTime() >= current)
+  // console.log('index', index, current, times.map((t) => new Date(t).getTime()))
+  if (index >= 0) {
+    const coords = poly.coordinates
+    const isFirst = index === 0
+    const beforeIndex = isFirst ? 0 : index - 1
+    const afterIndex = isFirst ? 0 : index
+    const beforeCoords = coords[beforeIndex]
+    const afterCoords = coords[afterIndex]
+    const before = turf.point(beforeCoords)
+    const after = turf.point(afterCoords)
+    const turfPath = turf.lineString([beforeCoords, afterCoords])
+    const len = turf.distance(before, after)
+    const beforeTime = timeVal(times[beforeIndex])
+    const afterTime = timeVal(times[afterIndex])
+    const timeDelta = afterTime - beforeTime
+    const proportion = (current - beforeTime) / timeDelta
+    const lenProp = len * proportion
+    const interpolated = isNaN(lenProp) ? before : turf.along(turfPath, lenProp)
+    const markerLoc = interpolated.geometry.coordinates
+    return markerLoc
+  } else {
+    return undefined
+  }
 }
 
 export const generateCurrentLocations = (features: Feature[], time: TimeState): Feature<Point>[] => {
   const temporalFeatures = features.filter(isTemporal)
   const pointFeatures = temporalFeatures.map((feature) => {
     const times = feature.properties?.times
-    const timeNow = time.current
-    const index = times.findIndex((time: string) => new Date(time).getTime() >= timeNow)
-    if (index >= 0) {
-      const poly = feature.geometry as MultiPoint
-      const markerLoc = calcInterpLocation(poly, times, time.current, index)
+    const poly = feature.geometry as MultiPoint
+    const markerLoc = calcInterpLocation(poly, times, time.current)
+    if (markerLoc) {
       const pointFeature: Feature<Point> = {
         type: 'Feature',
         id: feature.id,
@@ -58,8 +62,9 @@ export const generateCurrentLocations = (features: Feature[], time: TimeState): 
         }
       }
       return pointFeature
+    } else {
+      return undefined
     }
-    return undefined
   })
   const res = pointFeatures.filter((f) => f !== undefined) as Feature<Point>[]
   return res
