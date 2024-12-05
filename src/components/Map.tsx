5802f571b489708bad31ced9f977d3aca28aeef2
Ian Mayo <ian@planetmayo.com>
2024-12-05 17:44:50 +0000

    refactor point into its own class

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,13 +1,14 @@
-import { Feature, Geometry, Polygon } from "geojson";
-import { MapContainer,  GeoJSON } from 'react-leaflet'
+import { Feature, Geometry, Point, Polygon } from "geojson";
+import { MapContainer } from 'react-leaflet'
 import { PathOptions, StyleFunction, LatLngExpression, CircleMarker, LeafletMouseEvent } from 'leaflet'
-import { TRACK_TYPE, ZONE_TYPE } from "../constants";
+import { REFERENCE_POINT_TYPE, TRACK_TYPE, ZONE_TYPE } from "../constants";
 import Track from "./Track";
 import Zone from "./Zone";
 import { useCallback, useEffect, useMemo } from "react";
 import { useAppSelector } from "../app/hooks";
 import { useAppContext } from "../context/AppContext";
 import { generateCurrentLocations } from "../helpers/generateCurrentLocations";
+import { Point as DataPoint } from "./Point";
 
 interface CustomPathOptions extends PathOptions {
   radius?: number;
@@ -31,20 +32,7 @@ const Map: React.FC<MapProps> = ({ children }) => {
   const features = useAppSelector(state => state.featureCollection.features)
   const { selection, setSelection, time, setCurrentLocations } = useAppContext();
 
-  const styleFor: StyleFunction = (feature: Feature<Geometry, unknown> | undefined) => {
-    const res: CustomPathOptions = {}
-    if (feature) {
-      const feat = feature as Feature
-      if (feat?.properties?.color) {
-        res.color = feat.properties.color
-      }
-      if(selection.includes(feature.id as string)) {
-        res.color = '#aaa'
-      }
-    }
-    res.weight = 3
-    return res;
-  };
+
 
   useEffect(() => {
     if (time.current && features.length) {
@@ -69,21 +57,16 @@ const Map: React.FC<MapProps> = ({ children }) => {
     }
   }, [selection, setSelection])
 
-  const onTooltipClick = useCallback( (event: LeafletMouseEvent) => {
-    if (event.target.feature) {
-      onClickHandler(event.target.feature.id as string, event.originalEvent.altKey || event.originalEvent.ctrlKey)
-    }
-  }, [onClickHandler]);
-
   const featureFor = (feature: Feature): React.ReactElement => {
     switch(feature.properties?.dataType) {
     case TRACK_TYPE:
       return <Track key={feature.id} feature={feature} onClickHandler={onClickHandler}/> 
     case ZONE_TYPE:
       return <Zone key={feature.id} feature={feature as Feature<Polygon>} onClickHandler={onClickHandler}/>  
+    case REFERENCE_POINT_TYPE:
+      return <DataPoint key={feature.id} feature={feature as Feature<Point>} onClickHandler={onClickHandler} /> 
     default:
-      return <GeoJSON key={`${feature.id || 'index'}`} data={feature} style={styleFor} 
-      pointToLayer={(pointFeature, latlng) => createLabelledPoint(pointFeature, latlng, onTooltipClick)} /> 
+      throw new Error('Unknown feature type:' + feature.properties?.dataType)
     }
   }
 
