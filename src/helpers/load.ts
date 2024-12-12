660dbeece0d3bfc3b62e371cd01a8767976cd4ab
Ian Mayo <ian@planetmayo.com>
2024-12-12 10:47:41 +0000

    fix compiler error

--- src/helpers/load.ts
+++ src/helpers/load.ts
@@ -1,11 +1,12 @@
-import { FeatureCollection } from 'geojson';
+import { Feature, GeoJsonProperties, Geometry } from 'geojson';
 import { AppDispatch } from '../app/store';
 
-export const load = (text: string, features: FeatureCollection, dispatch: AppDispatch) => {
+export const load = (text: string, features: Feature<Geometry, GeoJsonProperties>[], dispatch: AppDispatch) => {
   try {
     const json = JSON.parse(text);
     if (json.type === 'FeatureCollection') {
       const newFeatures = json.features;
+      console.log('todo - check if this is a an existing feature and update it', features.length);
       dispatch({ type: 'featureCollection/featuresAdded', payload: newFeatures });
     } else {
       console.error('Invalid GeoJSON format');
