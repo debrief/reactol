ac3e9a3978a475d872233b9d56bac8709eb59c75
Ian Mayo <ian@planetmayo.com>
2024-12-13 09:32:35 +0000

    introduce drop-down with step sizes

--- src/components/TimeControl.tsx
+++ src/components/TimeControl.tsx
@@ -1,6 +1,6 @@
-import { Col, Row, Slider } from "antd";
-import { ClockCircleOutlined, FilterOutlined, PauseCircleOutlined, PlayCircleOutlined } from '@ant-design/icons';
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import { AutoComplete, Col, Row, Slider } from "antd";
+import { ClockCircleOutlined, FilterOutlined } from '@ant-design/icons';
+import React, { useEffect, useState } from "react";
 import { format } from 'date-fns';
 import { useAppContext } from "../context/AppContext";
 
@@ -10,6 +10,15 @@ export interface TimeProps {
   current?: number
   onTimeFilterChange?: (timePeriod: string) => void
 }
+
+const stepOptions = [
+  { value: "00h15m" },
+  { value: "00h30m" },
+  { value: "01h00m" },
+  { value: "02h00m" },
+  { value: "03h00m" },
+  { value: "06h00m" }
+]
 
 const steps = 100
 
@@ -37,9 +46,8 @@ const timeStr = (val: number | number[] | null, index?: number): string => {
 const TimeControl: React.FC<TimeProps> = ({start, end, current, onTimeFilterChange}) => {
   const { time, setTime } = useAppContext();
 
-  const [value, setValue] = useState<{ start: number, current: number, end: number }>({ start: 0, current: steps / 2, end: steps });
-  const [playing, setPlaying] = useState(false)
-  const timerRef = useRef<NodeJS.Timeout | null>(null)
+  const [value, setValue] = useState<{ start: number, end: number }>({ start: 0, end: steps });
+  const [stepTxt, setStepTxt] = useState<string>(stepOptions[1].value);
 
   useEffect(() => {
     const tStart = time.start || start
@@ -54,10 +62,11 @@ const TimeControl: React.FC<TimeProps> = ({start, end, current, onTimeFilterChan
     if (!Array.isArray(value)) {
       throw new Error('Expected an array to time control')
     }
-    const newValue = { start: value[0], current: value[1], end: value[2] }
+    const newValue = { start: value[0], end: value[1] }
     const unscaledValues = {
       start: unscaled(start, end, newValue.start),
-      current: unscaled(start, end, newValue.current),
+      step: 'alpha',
+      current: 0,
       end: unscaled(start, end, newValue.end)
     }
     setTime(unscaledValues)
@@ -68,48 +77,13 @@ const TimeControl: React.FC<TimeProps> = ({start, end, current, onTimeFilterChan
     }
   }
 
-  const PlayControl = useMemo(() => {
-    if (playing && value.current < steps) {
-      if (!timerRef.current) {
-        timerRef.current =  setInterval(() => {
-          let curTime = 0
-          setValue(prev => {
-            const newVal = { ...prev }
-            const newTime = newVal.current ? newVal.current + 20 : value.current
-            newVal.current = newTime
-            curTime = newTime
-            return newVal
-          })
-          const newTime = unscaled(start, end, curTime)
-          const unscaledValues = {
-            start: unscaled(start, end, value.start),
-            current: newTime,
-            end: unscaled(start, end, value.end)
-          }
-          setTime(unscaledValues)
-      
-        }, 1000)
-      }
-      return <PauseCircleOutlined onClick={() => setPlaying(false)}/>
-    } else {
-      if (timerRef.current) {
-        clearInterval(timerRef.current)
-        timerRef.current = null
-      }
-      return <PlayCircleOutlined onClick={() => setPlaying(true)}/>
-    }
-  }, [playing, value])
-
   return (
     <>  <Row>
-          <Col span={4}>
-            {PlayControl}
-          </Col>
           <Col span={20}>
             <Slider
               range={{draggableTrack: true}}
-              defaultValue={[value.start, value.current, value.end]}
-              value={[value.start, value.current, value.end]}
+              defaultValue={[value.start, value.end]}
+              value={[value.start, value.end]}
               tooltip={{open: false}}
               max={steps}
               min={0}
@@ -128,14 +102,21 @@ const TimeControl: React.FC<TimeProps> = ({start, end, current, onTimeFilterChan
         <thead>
           <tr>
             <th><FilterOutlined />Start</th>
-            <th><ClockCircleOutlined />Current</th>
+            <th><ClockCircleOutlined />Step</th>
             <th><FilterOutlined />End</th>
           </tr>
           </thead>
           <tbody>
             <tr style={{fontFamily: 'monospace'}}>
               <td>{timeStr(time.start)}</td>
-              <td style={{fontWeight: 'bold'}}>{timeStr(time.current)}</td>
+              <td><AutoComplete
+                style={{ width:100 }}
+                value={stepTxt}
+                onChange={(value, _option) => setStepTxt(value)}
+                defaultOpen={false}
+                options={stepOptions}
+                placeholder='00h30m'
+              /></td>
               <td>{timeStr(time.end)}</td>
             </tr>
           </tbody>
