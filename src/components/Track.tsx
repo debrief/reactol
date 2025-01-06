2153498fe385d979b0755554c39679ab503cce5a
Ian Mayo <ian@planetmayo.com>
2025-01-06 16:58:52 +0000

    Only show 20 positions/labels

--- src/components/Track.tsx
+++ src/components/Track.tsx
@@ -9,8 +9,6 @@ import { CoordInstance, filterTrack } from "../helpers/filterTrack";
 export interface TrackProps {
   feature: Feature 
   onClickHandler: {(id: string, modifier: boolean): void}
-  showCurrentLocation?: boolean
-  showTrackLine?: boolean
 }
 
 const colorFor = (feature: Feature<Geometry, unknown> | undefined, isSelected: boolean): string => {
@@ -26,12 +24,12 @@ const colorFor = (feature: Feature<Geometry, unknown> | undefined, isSelected: b
   return '#000';
 };
 
-const Track: React.FC<TrackProps> = ({feature, onClickHandler, showTrackLine = true}) => {
+const Track: React.FC<TrackProps> = ({feature, onClickHandler}) => {
   const { selection, time } = useAppContext()
   const isSelected = selection.includes(feature.id as string)
 
 
-  const trackCoords = useMemo(() => {
+  const trackCoords: CoordInstance[] = useMemo(() => {
     if (time && feature.properties?.times) {
       const coords = (feature.geometry as MultiPoint).coordinates
       const times = feature.properties.times
@@ -40,8 +38,10 @@ const Track: React.FC<TrackProps> = ({feature, onClickHandler, showTrackLine = t
     } else {
       const coords = (feature.geometry as MultiPoint).coordinates
       const times = feature.properties?.times
+      const timeFreq = Math.floor(times.length / 20)
+
       if (times && times.length) {
-        return times.map((time: string, index: number) => {return {pos:[coords[index][1], coords[index][0]],time: format(time, "ddHHmm'Z'")}})
+        return times.map((time: string, index: number) => {return {pos:[coords[index][1], coords[index][0]],time: format(time, "ddHHmm'Z'"), timeVisible: index % timeFreq === 0}})
       } else {
         return coords.map((coord: number[]) => {return {pos:[coord[1], coord[0]],time: ''}})
       }
@@ -54,12 +54,12 @@ const Track: React.FC<TrackProps> = ({feature, onClickHandler, showTrackLine = t
 
   return (
     <>
-      { showTrackLine && <Polyline key={feature.id + '-line-' + isSelected} eventHandlers={{click: onclick}} positions={trackCoords.map((val: CoordInstance) => val.pos)} weight={2} color={colorFor(feature, isSelected)}/>}
-      { showTrackLine && trackCoords.length && <CircleMarker key={feature.id + '-start-line-' + isSelected} center={trackCoords[0].pos}  color={colorFor(feature, isSelected)} radius={0}>
+      { <Polyline key={feature.id + '-line-' + isSelected} eventHandlers={{click: onclick}} positions={trackCoords.map((val: CoordInstance) => val.pos)} weight={2} color={colorFor(feature, isSelected)}/>}
+      { trackCoords.length && <CircleMarker key={feature.id + '-start-line-' + isSelected} center={trackCoords[0].pos}  color={colorFor(feature, isSelected)} radius={0}>
         <Tooltip key={feature.id + '-start-name-' + isSelected} 
           direction='left' opacity={1} permanent>{feature.properties?.name}</Tooltip>
       </CircleMarker> }
-      { showTrackLine && trackCoords.map((item: CoordInstance, index: number) => 
+      { trackCoords.filter((item) => item.timeVisible).map((item: CoordInstance, index: number) => 
         <CircleMarker key={feature.id + '-point-' + index} center={item.pos} radius={3} color={colorFor(feature, isSelected)} eventHandlers={{click: onclick}}>
           {feature.properties?.times && <Tooltip  key={feature.id + '-tip-' + index} offset={[0, -20]} direction="center" opacity={1} permanent>
             {item.time}
