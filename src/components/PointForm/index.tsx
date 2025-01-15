6ff936b6931e2bb8f66702a5be9ef6f489894814
Ian Mayo <ian@planetmayo.com>
2025-01-15 14:07:37 +0000

    displaying/storing time values

--- src/components/PointForm/index.tsx
+++ src/components/PointForm/index.tsx
@@ -1,8 +1,10 @@
 import { Feature, Point, Position } from "geojson";
 import { PointProps } from "../../types";
-import { Checkbox, ColorPicker, Form, Input } from "antd";
+import { Checkbox, ColorPicker, DatePicker, Form, Input } from "antd";
 import { Color } from "antd/es/color-picker";
-
+import dayjs from 'dayjs';
+import type { Dayjs } from 'dayjs';
+import { useEffect, useState } from "react";
 
 export interface PointFormProps {
   point: Feature<Point, PointProps>
@@ -11,26 +13,44 @@ export interface PointFormProps {
 
 type PointsWithCoords = PointProps & {
   coords: Position
+  dTime: Dayjs
+}
+
+const convert = (point: Feature<Point, PointProps>): PointsWithCoords => {
+  return {...point.properties, coords: point.geometry.coordinates, dTime: dayjs(point.properties.time)} as PointsWithCoords
 }
 
 export const PointForm: React.FC<PointFormProps> = ({point, onChange}) => {
+  const [values, setValues] = useState<PointsWithCoords | null>(null)
+
+  useEffect(() => {
+    setValues(convert(point))
+  }, [point, setValues])
 
   const localChange = (values: Partial<PointsWithCoords>) => {
     if (values.color) {
       values.color = (values.color as unknown as Color).toHexString()
+    }
+    if (values.dTime) {
+      values.time = (values.dTime as Dayjs).toISOString()
+      delete values.dTime
     }
     const updatedProps = {...point.properties, ...values}
     const newVal = {...point, properties: updatedProps} as Feature<Point, PointProps>
     onChange(newVal)
   }
 
+  if (!values) {
+    return null
+  }
+
   return (
     <Form
       name='createTrack'
       labelCol={{ span: 6 }}
-      wrapperCol={{ span: 18 }}
+      wrapperCol={{ span: 14 }}
       style={{ maxWidth: 400 }}
-      initialValues={ {...point.properties, coords: point.geometry.coordinates} as PointsWithCoords }
+      initialValues={values}
       autoComplete='off'
       onValuesChange={localChange}
     >
@@ -50,12 +70,18 @@ export const PointForm: React.FC<PointFormProps> = ({point, onChange}) => {
         <Checkbox />
       </Form.Item>
       <Form.Item<PointsWithCoords>
-        name="color"
         label="Color"
+        name="color"
         rules={[{ required: true, message: 'color is required!' }]}>
         <ColorPicker format='hex' trigger='click' />
+      </Form.Item>
+      <Form.Item<PointsWithCoords>
+        label="Time"
+        name="dTime">
+        <DatePicker showTime format={'MMM DDHHmm'} />
       </Form.Item>
 
     </Form>
   )
 }
+
