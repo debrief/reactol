085cf8b9c62d8a259bc031b0c7f65dbe7f17c392
Ian Mayo <ian@planetmayo.com>
2025-01-17 17:26:05 +0000

    simplify types

--- src/components/PointForm/index.tsx
+++ src/components/PointForm/index.tsx
@@ -1,48 +1,58 @@
-import { Feature, Point, Position } from "geojson";
-import { PointProps } from "../../types";
+import { Feature, Point } from "geojson";
 import { Checkbox, ColorPicker, DatePicker, Form, Input } from "antd";
 import { Color } from "antd/es/color-picker";
+import { useEffect, useState } from "react";
 import dayjs from 'dayjs';
 import type { Dayjs } from 'dayjs';
-import { useEffect, useState } from "react";
+import { PointProps } from "../../types";
 
 export interface PointFormProps {
   point: Feature<Point, PointProps>
   onChange: (point: Feature<Point, PointProps>) => void
 }
 
-type PointsWithCoords = PointProps & {
-  coordinates: Position
+/** swap the time string a parameter of the expected type */
+type FormTypeProps = Omit<PointProps, 'time'> & {
   dTime: Dayjs
 }
 
-const convert = (point: Feature<Point, PointProps>): PointsWithCoords => {
-  return {...point?.properties, 
-    coordinates: point?.geometry.coordinates, 
-    dTime: dayjs(point?.properties.time)} as PointsWithCoords
+const convert = (point: Readonly<PointProps>): FormTypeProps=> {
+  const oldVal = point
+  const newVal =  JSON.parse(JSON.stringify(point)) as FormTypeProps
+  if (oldVal.time) {
+    newVal.dTime = dayjs(oldVal.time)
+    delete (newVal as Partial<PointProps>).time
+  }
+  return newVal
+}
+
+const convertBack = (point: Readonly<FormTypeProps>): PointProps => {
+  const oldVal = point 
+  const newVal = JSON.parse(JSON.stringify(point)) as PointProps
+  if (point.dTime) {
+    newVal.time = oldVal.dTime.toISOString() 
+    delete (newVal as Partial<FormTypeProps>).dTime
+  }
+  return newVal
 }
 
 export const PointForm: React.FC<PointFormProps> = ({point, onChange}) => {
-  const [state, setState] = useState<PointsWithCoords | null>(null)
+  const [state, setState] = useState<FormTypeProps | null>(null)
 
   useEffect(() => {
     if (point) {
-      const newState = convert(point)
-      setState(newState)
+      setState(convert(point.properties))
     }
   }, [point, setState])
 
-  const localChange = (values: Partial<PointsWithCoords>) => {
+  const localChange = (values: Partial<FormTypeProps>) => {
     if (values.color) {
       values.color = (values.color as unknown as Color).toHexString()
     }
-    if (values.dTime) {
-      values.time = (values.dTime as Dayjs).toISOString()
-      delete values.dTime
-    }
-    const updatedProps = {...point.properties, ...values} as PointsWithCoords
-    const updatedPoint = {...point, properties: updatedProps} as Feature<Point, PointProps>
-    onChange(updatedPoint)
+    const updatedProps= {...state, ...values} as FormTypeProps
+    const convertedProps = convertBack(updatedProps)
+    const res = {...point, properties: convertedProps}
+    onChange(res)
   }
 
   if (!state) {
@@ -61,38 +71,34 @@ export const PointForm: React.FC<PointFormProps> = ({point, onChange}) => {
       autoComplete='off'
       onValuesChange={localChange}
       variant='filled'
-      size='middle'
-    >
-      <Form.Item<PointsWithCoords>
+      size='middle'>
+      <Form.Item<FormTypeProps>
         label='Name'
         name='name'
         style={itemStyle}
-        rules={[{ required: true, message: 'Please enter track name!' }]}
-      >
+        rules={[{ required: true, message: 'Please enter track name!' }]}>
         <Input/>
       </Form.Item>
-      <Form.Item<PointsWithCoords>
+      <Form.Item<FormTypeProps>
         label='Visible'
-        name='visible'
+        name={'visible'}
         style={itemStyle}
-        valuePropName="checked" 
-      >
+        valuePropName="checked" >
         <Checkbox style={{alignItems: 'start'}}  />
       </Form.Item>
-      <Form.Item<PointsWithCoords>
+      <Form.Item<FormTypeProps>
         label="Color"
-        name="color"
+        name='color'
         style={itemStyle}
         rules={[{ required: true, message: 'color is required!' }]}>
         <ColorPicker format='hex' trigger='click' />
       </Form.Item>
-      <Form.Item<PointsWithCoords>
+      <Form.Item<FormTypeProps>
         label="Time"
         style={itemStyle}
-        name="dTime">
+        name='dTime'>
         <DatePicker showTime format={'MMM DDHHmm'} />
       </Form.Item>
-
     </Form>
   )
 }
