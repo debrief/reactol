ceaa28a36cb5ec38e19cd0c708b864226adbc7ff
Ian Mayo <ian@planetmayo.com>
2025-01-29 16:09:46 +0000

    editable tabs

--- src/components/Documents/index.tsx
+++ src/components/Documents/index.tsx
@@ -1,15 +1,13 @@
-import { Key, useState } from 'react'
+import { useState } from 'react'
 import App from '../../App'
-import { Tabs, TabsProps } from 'antd'
+import { Button, Tabs, TabsProps } from 'antd'
 
 const Documents = () => {
-  const [tabs, setTabs] = useState<TabsProps['items']>([])
-  const [activeTab, setActiveTab] = useState<Key | null>(null)
+  const [tabs, setTabs] = useState<NonNullable<TabsProps['items']>>([])
+  const [activeTab, setActiveTab] = useState<string | undefined>(undefined)
 
   const createNewDocument = () => {
-    if (!tabs) {
-      return
-    }
+    console.log('create new document')
     const newTab = { key: '' + Date.now(), label: `Untitled ${tabs.length + 1}`, children: <App/> }
     setTabs([...tabs, newTab])
     setActiveTab(newTab.key)
@@ -17,7 +15,7 @@ const Documents = () => {
 
   const openExistingDocument = async () => {
     const file = await window.electron.openFile()
-    if (file && tabs) {
+    if (file) {
       // TODO give `App` a prop repsesenting the file content. It will
       // verify this is GeoJSON, and load it into the store.
       const newTab = { key: '' + Date.now(), label: file.filePath.split('/').pop()!, children: <App/> }
@@ -26,39 +24,53 @@ const Documents = () => {
     }
   }
 
-  const saveDocument = async () => {
-    // if (activeTab !== null) {
-    //   const doc = tabs.find(t => t.id === activeTab)
-    //   if (doc) {
-    //     let filePath = doc.path
+  //const saveDocument = async () => {
+  // if (activeTab !== null) {
+  //   const doc = tabs.find(t => t.id === activeTab)
+  //   if (doc) {
+  //     let filePath = doc.path
 
-    //     if (!filePath) {
-    //       const result = await window.electron.saveFileDialog()
-    //       if (result?.filePath) {
-    //         filePath = result.filePath
-    //       } else {
-    //         return
-    //       }
-    //     }
+  //     if (!filePath) {
+  //       const result = await window.electron.saveFileDialog()
+  //       if (result?.filePath) {
+  //         filePath = result.filePath
+  //       } else {
+  //         return
+  //       }
+  //     }
 
-    //     await window.electron.saveFile(filePath, doc.content)
-    //     setTabs(tabs.map(t => (t.id === activeTab ? { ...t, path: filePath, name: filePath.split('/').pop()! } : t)))
-    //   }
-    // }
-  }
+  //     await window.electron.saveFile(filePath, doc.content)
+  //     setTabs(tabs.map(t => (t.id === activeTab ? { ...t, path: filePath, name: filePath.split('/').pop()! } : t)))
+  //   }
+  // }
+  //}
 
   const onTabChange = (key: string) => {
     setActiveTab(key)
   }
+  
+  const operations = <Button onClick={openExistingDocument}>Open</Button>
+
+  function onEdit(e: string | React.MouseEvent | React.KeyboardEvent, action: 'add' | 'remove'): void {
+    console.log('edit', e, action)
+    switch (action) {
+    case 'add':
+      createNewDocument()
+      break
+    case 'remove':     
+      setTabs(tabs.filter(t => t.key !== e))
+      break
+    }    
+  }
 
   return (
     <div>
-      <button onClick={createNewDocument}>New</button>
-      <button onClick={openExistingDocument}>Open</button>
-      <button onClick={saveDocument} disabled={activeTab === null}>Save</button>
-
-      <Tabs onChange={onTabChange} items={tabs} />
-
+      <Tabs tabBarExtraContent={operations} 
+        type='editable-card'
+        activeKey={activeTab}
+        onChange={onTabChange} items={tabs} 
+        onEdit={onEdit}/>
+        
     </div>
   )
 }
