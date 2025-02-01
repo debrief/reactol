8bcbad955d5b864a435d87628225036295c1a6fd
Ian Mayo <ian@planetmayo.com>
2025-02-01 17:29:05 +0000

    Arrange delete button

--- src/components/CoreForm/index.tsx
+++ src/components/CoreForm/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Tooltip } from 'antd'
+import { Button, Col, Flex, Row, Tooltip } from 'antd'
 
 export interface CoreFormProps {
   children: React.ReactNode
@@ -12,17 +12,25 @@ export const CoreForm: React.FC<CoreFormProps> = ({children, onReset, onSave,  o
   return (
     <div style={{marginTop: '5px'}}>
       {children}
-      <Flex gap='small' justify='end' wrap style={{height: '1em', borderTop: '2px solid #ccc', paddingTop: '5px'}}>
-        <Tooltip title='Delete feature' placement="top">
-          <Button danger type='primary' style={{marginRight: '20px'}} disabled={false} size='small' onClick={onDelete}>Delete</Button>
-        </Tooltip>
-        <Tooltip title='Reset all values' placement="top">
-          <Button disabled={!formDirty} size='small' onClick={onReset}>Reset</Button>
-        </Tooltip>
-        <Tooltip title='Save edits' placement="top">
-          <Button type='primary' disabled={!formDirty} size='small' onClick={onSave}>Save</Button>
-        </Tooltip>
-      </Flex>
+      <Row>
+        <Col span={8}>
+          <Tooltip title='Delete feature' placement="top">
+            <Flex gap='small' justify='start' wrap style={{height: '1em', borderTop: '2px solid #ccc', paddingTop: '5px'}}>
+              <Button danger type='primary' disabled={false} size='small' onClick={onDelete}>Delete</Button>
+            </Flex>
+          </Tooltip>
+        </Col>
+        <Col span={16}>
+          <Flex gap='small' justify='end' wrap style={{height: '1em', borderTop: '2px solid #ccc', paddingTop: '5px'}}>
+            <Tooltip title='Reset all values' placement="top">
+              <Button disabled={!formDirty} size='small' onClick={onReset}>Reset</Button>
+            </Tooltip>
+            <Tooltip title='Save edits' placement="top">
+              <Button type='primary' disabled={!formDirty} size='small' onClick={onSave}>Save</Button>
+            </Tooltip>
+          </Flex> 
+        </Col>
+      </Row>
     </div>
   )
 }
