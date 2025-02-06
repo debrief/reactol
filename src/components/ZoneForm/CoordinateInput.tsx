1c53a207cafd23ac599ec81a878612be9e5b2987
Ian Mayo <ian@planetmayo.com>
2025-02-06 17:28:14 +0000

    Toggle working

--- src/components/ZoneForm/CoordinateInput.tsx
+++ src/components/ZoneForm/CoordinateInput.tsx
@@ -1,14 +1,15 @@
-import { Col, Row } from 'antd'
+import { Col, Row, Switch } from 'antd'
 import { CoordinateElementInput } from './CoordinateElement'
-
+import { useState } from 'react'
 
 interface CoordinateInputProps {
   value?: [number, number]
   onChange?: (value: [number, number]) => void
 }
 
-
 export const CoordinateInput: React.FC<CoordinateInputProps> = ({ value, onChange }) => {
+  const [shortFormat, setShortFormat] = useState<boolean>(true)
+
   const handleChange = (newValue: number, isLatitude: boolean) => {
     const result: [number, number] = isLatitude ? [value?.[0] || 0, newValue] : [newValue, value?.[1] || 0]
     onChange && onChange(result)
@@ -20,12 +21,35 @@ export const CoordinateInput: React.FC<CoordinateInputProps> = ({ value, onChang
   
   return (
     <>
+      <Row gutter={[16, 8]} align="middle">
+      </Row>
       <Row gutter={[16, 0]}>
-        <Col span={12}>
-          <CoordinateElementInput style={{width: '90px'}} value={value?.[1] || 0} onChange={handleChange} isLatitude/>
+        <Col span={9}>
+          <CoordinateElementInput 
+            style={{width: '90px'}} 
+            value={value?.[1] || 0} 
+            onChange={handleChange} 
+            isLatitude
+            shortFormat={shortFormat}
+          />
         </Col>
-        <Col span={12}>
-          <CoordinateElementInput style={{width: '100px'}} value={value?.[0] || 0} onChange={handleChange} isLatitude={false}/>
+        <Col span={9}>
+          <CoordinateElementInput 
+            style={{width: '100px'}} 
+            value={value?.[0] || 0} 
+            onChange={handleChange} 
+            isLatitude={false}
+            shortFormat={shortFormat}
+          />
+        </Col>
+        <Col span={6}>
+          <Switch
+            size="small"
+            checked={shortFormat}
+            onChange={setShortFormat}
+            checkedChildren="DM"
+            unCheckedChildren="DMS"
+          />
         </Col>
       </Row>
     </>
