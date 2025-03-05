3e791499dfd66c0d6df166695d892ba86dc5bc1e
Ian Mayo <ian@planetmayo.com>
2025-03-05 13:02:30 +0000

    make sample data meet schema

--- src/data/track2.ts
+++ src/data/track2.ts
@@ -12,6 +12,8 @@ const track: Feature<LineString, TrackProps> = {
     env: 'sub',
     labelInterval: 2 * 60 * 60 * 1000,
     symbolInterval: 30 * 60 * 1000,
+    initialMonth: 11,
+    initialYear: 2024,
     times: [
       '2024-11-14T16:16:53.662Z',
       '2024-11-14T16:17:53.662Z',
