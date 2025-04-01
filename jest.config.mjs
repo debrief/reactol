9c2e9d9b04c605fa7acd3fb509dd1536a173c77f
Ian Mayo <ian@planetmayo.com>
2025-04-01 09:17:38 +0100

    tell jest not to run e2e tests

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,17 +1,28 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 export default {
   testEnvironment: "node",
+  // Only run tests in the src directory
+  testMatch: [
+    "<rootDir>/src/**/*.test.[jt]s?(x)",
+    "<rootDir>/src/**/__tests__/**/*.[jt]s?(x)"
+  ],
+  // Explicitly ignore tests in the Playwright e2e directory
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/e2e/",
+    "<rootDir>/tests/"
+  ],
   moduleNameMapper: {
-    '^leaflet$': '<rootDir>/src/mock/leaflet.js',
+    '^leaflet$': '<rootDir>/src/mock/leaflet.js'
   },
   transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
+    "^.+.tsx?$": ["ts-jest",{}]
   },
   globals: {
     'ts-jest': {
       tsconfig: {
         esModuleInterop: true
-      },
-    },
-  },
-};
+      }
+    }
+  }
+}
