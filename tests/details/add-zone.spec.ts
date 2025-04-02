1e5dbafd9e82c446ba198382725b14ba8cbf8149
Ian Mayo <ian@planetmayo.com>
2025-04-02 12:44:01 +0100

    fix tests

--- tests/details/add-zone.spec.ts
+++ tests/details/add-zone.spec.ts
@@ -1,11 +1,17 @@
 import { test } from '@playwright/test'
 
 test('test', async ({ page }) => {
-  const newZone = 'New Polygon'
   await page.goto('/')
   await page.getByRole('button', { name: 'Sample plot' }).click()
   await page.getByRole('textbox').fill('add zone')
-  await page.getByRole('button', { name: 'OK' }).click()
+  await page.getByRole('textbox').press('Enter')
+
+  await page.waitForTimeout(100)
+
+  const newZone = 'New Polygon'
+  // collapse the `Units` node
+  const unitsNode = page.locator('span:has-text("Units")').first()
+  await unitsNode.click({ force: true })
   await page.getByRole('treeitem', { name: 'plus-square plus-circle Zones' }).locator('svg').nth(1).click()
   await page.getByText('Polygon').click()
   await page.getByRole('textbox', { name: '* Name :' }).click()
