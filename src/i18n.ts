047c0f5bfb27d0c83d0d8a2b9aa8b6872d398537
Ian Mayo <ian@planetmayo.com>
2025-04-04 17:24:02 +0000

    add nl translations

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,6 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import enTranslations from './locales/en.json';
 import itTranslations from './locales/it.json';
+import nlTranslations from './locales/nl.json';
 
 i18n
   .use(initReactI18next)
@@ -9,6 +10,7 @@ i18n
     resources: {
       en: { translation: enTranslations },
       it: { translation: itTranslations },
+      nl: { translation: nlTranslations }
     },
     lng: "en",
     fallbackLng: "en",
